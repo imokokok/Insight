dc81b6bfb81845ae103f0c36463bf2a20a580ea3
imokokok <imokokok123@gmail.com>
2026-03-17 00:25:59 +0800

    chore: 清理未使用的导出和 dead code
    - 删除 src/lib/api/middleware/index.ts 中 24 个未使用的导出
    - 删除 src/lib/api/validation/index.ts 中 14 个未使用的导出
    - 删除 src/lib/api/versioning/index.ts 中 11 个未使用的导出
    - 删除 src/lib/oracles/__mocks__/index.ts 中 9 个未使用的导出
    - 删除 src/hooks/useAPI3WebSocket.ts 中未使用的 hooks (useAPI3Prices, useAPI3Realtime)
    - 删除 src/hooks/useOracleData.ts 中未使用的 hook (useMultiplePrices)
    - 清理组件库中未使用的类型导出
    
    总计清理 60+ 个未使用的导出

--- src/components/oracle/common/index.ts
+++ src/components/oracle/common/index.ts
@@ -3,44 +3,22 @@ export { PageHeader } from './PageHeader';
 export { TabNavigation, type TimeRange } from './TabNavigation';
 export { OraclePageTemplate } from './OraclePageTemplate';
 export { FloatingActionButton } from './FloatingActionButton';
-export type { FloatingActionButtonProps } from './FloatingActionButton';
 export { MoreOptionsDropdown } from './MoreOptionsDropdown';
 export { RealtimeUpdateControl } from './RealtimeUpdateControl';
-export type {
-  UpdateInterval,
-  ConnectionStatus,
-  RealtimeUpdateControlProps,
-} from './RealtimeUpdateControl';
 export { SnapshotManager } from './SnapshotManager';
 export { SnapshotComparison } from './SnapshotComparison';
 export { AnomalyAlert } from './AnomalyAlert';
 export { AnomalyMarker } from './AnomalyMarker';
 export type { AnomalyPoint } from './AnomalyMarker';
-export { VolatilityAlert } from './VolatilityAlert';
 export { ConfidenceScore } from './ConfidenceScore';
-export type {
-  ConfidenceScoreProps,
-  ConfidenceData,
-  DimensionScore,
-  TrendDataPoint as ConfidenceTrendDataPoint,
-  ConfidenceLevel,
-} from './ConfidenceScore';
 export { ConfidenceIntervalDisplay } from './ConfidenceIntervalDisplay';
 export { default as DataQualityIndicator } from './DataQualityIndicator';
 export { DataQualityScoreCard } from './DataQualityScoreCard';
-export type {
-  DataQualityScoreCardProps,
-  FreshnessData,
-  CompletenessData,
-  ReliabilityData,
-  QualityLevel,
-} from './DataQualityScoreCard';
 export { UMADataQualityScoreCard } from './UMADataQualityScoreCard';
 export { UMAScoreExplanationModal } from './UMAScoreExplanationModal';
 export { CoveragePoolTimeline } from './CoveragePoolTimeline';
 export { DapiPriceDeviationMonitor } from './DapiPriceDeviationMonitor';
 export { FirstPartyOracleAdvantages } from './FirstPartyOracleAdvantages';
-export type { FirstPartyOracleAdvantages as FirstPartyAdvantages } from './FirstPartyOracleAdvantages';
 export { IntegratedProtocols } from './IntegratedProtocols';
 export { DataSourceCoverage } from './DataSourceCoverage';
 export { DataSourceCredibility } from './DataSourceCredibility';
@@ -48,55 +26,20 @@ export { DataSourceTraceability } from './DataSourceTraceability';
 export { PublisherList } from './PublisherList';
 export { PublisherReliabilityScore } from './PublisherReliabilityScore';
 export { OraclePerformanceRanking } from './OraclePerformanceRanking';
-export type { OraclePerformanceData, RankingChange } from './OraclePerformanceRanking';
 export { PerformanceGauge, PerformanceGaugeGroup } from './PerformanceGauge';
-export type { PerformanceGaugeProps } from './PerformanceGauge';
 export { PriceAccuracyStats } from './PriceAccuracyStats';
-export { PriceDeviationMonitor } from './PriceDeviationMonitor';
 export { ExtremeMarketAnalysis } from './ExtremeMarketAnalysis';
 export { StakingCalculator } from './StakingCalculator';
-export { GasFeeComparison, type GasFeeData } from './GasFeeComparison';
-export { LatencyAnalysis } from './LatencyAnalysis';
-export type {
-  LatencyDataPoint,
-  TrendDataPoint,
-  ChainLatencyData,
-  AnomalyData,
-  LatencyStats,
-} from './LatencyAnalysis';
+export { GasFeeComparison } from './GasFeeComparison';
 export { DisputeAmountDistribution } from './DisputeAmountDistribution';
 export { DisputeEfficiencyAnalysis } from './DisputeEfficiencyAnalysis';
 export { RequestTypeDistribution } from './RequestTypeDistribution';
-export type { RequestTypeData, RequestTypeDistributionProps } from './RequestTypeDistribution';
-export { ValidatorEarningsBreakdown } from './ValidatorEarningsBreakdown';
-export type {
-  ValidatorEarningsBreakdownProps,
-  EarningsSourceBreakdown,
-  EfficiencyMetrics,
-} from './ValidatorEarningsBreakdown';
 export { CrossChainPriceConsistency } from './CrossChainPriceConsistency';
-export type { ChainPriceData, CrossChainPriceConsistencyProps } from './CrossChainPriceConsistency';
 export { BandCrossChainPriceConsistency } from './BandCrossChainPriceConsistency';
-export type {
-  BandChainPriceData,
-  BandCrossChainPriceConsistencyProps,
-} from './BandCrossChainPriceConsistency';
 export { ChainEventMonitor } from './ChainEventMonitor';
-export type { ChainEventMonitorProps } from './ChainEventMonitor';
-export { ChartAnnotations } from './ChartAnnotations';
-export type {
-  ChartAnnotation,
-  AnnotationType,
-  ImportanceLevel,
-  ChartAnnotationsProps,
-} from './ChartAnnotations';
+export { DataFreshnessIndicator } from './DataFreshnessIndicator';
+export { RiskScoreCard } from './RiskScoreCard';
+export { SecurityTimeline } from './SecurityTimeline';
+export { MitigationMeasuresGrid } from './MitigationMeasuresGrid';
 export { LoadingState } from './LoadingState';
 export { ErrorFallback } from './ErrorFallback';
-export { DataFreshnessIndicator } from './DataFreshnessIndicator';
-export type { DataFreshnessIndicatorProps } from './DataFreshnessIndicator';
-export { RiskScoreCard } from './RiskScoreCard';
-export type { RiskScoreCardProps } from './RiskScoreCard';
-export { SecurityTimeline } from './SecurityTimeline';
-export type { SecurityTimelineProps } from './SecurityTimeline';
-export { MitigationMeasuresGrid } from './MitigationMeasuresGrid';
-export type { MitigationMeasuresGridProps } from './MitigationMeasuresGrid';
--- src/components/oracle/panels/index.ts
+++ src/components/oracle/panels/index.ts
@@ -1,68 +1,24 @@
 export { DataQualityPanel } from './DataQualityPanel';
-export type {
-  DataQualityPanelProps,
-  DataQualityConfig,
-  PriceDeviationData,
-  LatencyDistributionData,
-  LatencyMetrics,
-  DataSourceReliability,
-  QualityScore,
-  QualityStatus,
-} from './DataQualityPanel';
 export { RiskAssessmentPanel } from './RiskAssessmentPanel';
 export { AccuracyAnalysisPanel } from './AccuracyAnalysisPanel';
 export { AirnodeDeploymentPanel } from './AirnodeDeploymentPanel';
-export type { AirnodeDeployments } from './AirnodeDeploymentPanel';
-export { AnomalyStatsPanel } from './AnomalyStatsPanel';
 export { ConfidenceAlertPanel } from './ConfidenceAlertPanel';
-export type { ConfidenceAlert, AlertType, AlertSeverity } from './ConfidenceAlertPanel';
 export { CoveragePoolPanel } from './CoveragePoolPanel';
-export type { CoveragePoolData } from './CoveragePoolPanel';
 export { CrossChainPanel } from './CrossChainPanel';
-export type { CrossChainPanelProps, ChainDataRequest, CrossChainStats } from './CrossChainPanel';
 export { DapiCoveragePanel } from './DapiCoveragePanel';
-export type { DapiCoverage } from './DapiCoveragePanel';
 export { DataSourceTraceabilityPanel } from './DataSourceTraceabilityPanel';
 export { DisputeResolutionPanel } from './DisputeResolutionPanel';
-export { DisputeVotingPanel, generateMockVotingData } from './DisputeVotingPanel';
-export type {
-  DisputeVotingPanelProps,
-  DisputeVotingData,
-  ValidatorVote,
-  VotePosition,
-} from './DisputeVotingPanel';
+export { DisputeVotingPanel } from './DisputeVotingPanel';
 export { EcosystemPanel } from './EcosystemPanel';
 export { MarketDataPanel, type MarketDataConfig } from './MarketDataPanel';
-export { MarketDataPanelRealtime } from './MarketDataPanelRealtime';
 export { NetworkHealthPanel, type NetworkDataConfig } from './NetworkHealthPanel';
-export { NodeReputationPanel } from './NodeReputationPanel';
-export type { NodeReputationData, NodeType } from './NodeReputationPanel';
 export { PublisherAnalysisPanel } from './PublisherAnalysisPanel';
 export { PublisherContributionPanel } from './PublisherContributionPanel';
 export { StakingMetricsPanel } from './StakingMetricsPanel';
 export { ValidatorAnalyticsPanel } from './ValidatorAnalyticsPanel';
 export { ValidatorPanel } from './ValidatorPanel';
-export type { ValidatorPanelProps, SortField, SortDirection, FilterStatus } from './ValidatorPanel';
-export { DataQualityScorePanel } from './DataQualityScorePanel';
-export type {
-  DataQualityScorePanelProps,
-  QualityDimension,
-  HistoricalScore,
-  QualityAlert,
-} from './DataQualityScorePanel';
-export { RedStoneMetricsPanel } from './RedStoneMetricsPanel';
-export type { RedStoneMetrics, RedStoneProviderInfo } from '@/lib/oracles/redstone';
 export { BandCrossChainPanel } from './BandCrossChainPanel';
-export { CosmosEcosystemPanel } from './CosmosEcosystemPanel';
 export { BandRiskAssessmentPanel } from './BandRiskAssessmentPanel';
-export { DIADataSourcesPanel } from './DIADataSourcesPanel';
-export { DIADataTransparencyPanel } from './DIADataTransparencyPanel';
-export { DIACrossChainCoveragePanel } from './DIACrossChainCoveragePanel';
-export { DIADataSourceVerificationPanel } from './DIADataSourceVerificationPanel';
-export { DIANFTDataPanel } from './DIANFTDataPanel';
-export { DIAStakingPanel } from './DIAStakingPanel';
-export { DIADataFeedsPanel } from './DIADataFeedsPanel';
-export { DIAEcosystemPanel } from './DIAEcosystemPanel';
 export { DIARiskAssessmentPanel } from './DIARiskAssessmentPanel';
 export { ChainlinkNodesPanel } from './ChainlinkNodesPanel';
 export { ChainlinkDataFeedsPanel } from './ChainlinkDataFeedsPanel';
@@ -77,3 +33,9 @@ export { BandDataFeedsPanel } from './BandDataFeedsPanel';
 export { BandValidatorsPanel } from './BandValidatorsPanel';
 export { UMANetworkPanel } from './UMANetworkPanel';
 export { UMARiskPanel } from './UMARiskPanel';
+export { DIANFTDataPanel } from './DIANFTDataPanel';
+export { DIAStakingPanel } from './DIAStakingPanel';
+export { DIADataFeedsPanel } from './DIADataFeedsPanel';
+export { DIAEcosystemPanel } from './DIAEcosystemPanel';
+export { DataQualityScorePanel } from './DataQualityScorePanel';
+export { CosmosEcosystemPanel } from './CosmosEcosystemPanel';
